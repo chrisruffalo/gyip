e323003db3ad9b14f95f881a06c0760ef1db2dc0
Chris Ruffalo <chris.ruffalo@gmail.com>
2017-10-31 09:15:03 -0400

    added command test

---
+++ command/command_test.go
@@ -0,0 +1,25 @@
+package command
+
+import "testing"
+
+func TestCommandFactory(t *testing.T) {
+	data := []struct {
+		command string
+		eType   CommandType
+	}{
+		{"", NOOP},
+		{"RR", RR},
+		{"F1", FAIL},
+		{"F50", FAIL},
+		{"F28984", NOOP},
+		{"FAB", NOOP},
+	}
+
+	for _, item := range data {
+		cmd := New(item.command)
+		if item.eType != cmd.Type() {
+			t.Errorf("Command string '%s' did not return expected command type (was: %v, expected %v)", item.command, cmd.Type(), item.eType)
+		}
+	}
+
+}
