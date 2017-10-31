f8ac786b319cb7fb299d39ff6002afb2d9ddae26
Chris Ruffalo <chris.ruffalo@gmail.com>
2017-10-31 13:01:14 -0400

    fixed a test error comment

--- command/noop_test.go
+++ command/noop_test.go
@@ -13,6 +13,6 @@ func TestNoop(t *testing.T) {
 		t.Errorf("Single input IP list did not produce expected result")
 	}
 	if ttl != defaultTTL {
-		t.Errorf("No-transform did not return the same ttl (was %d, expected %d)", ttl, defaultTTL)
+		t.Errorf("Noop did not return the same ttl (was %d, expected %d)", ttl, defaultTTL)
 	}
 }
