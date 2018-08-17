6964171e81cc0bc16ef9e8fe242ffc092538c461
Chris Ruffalo <chris.ruffalo@gmail.com>
2018-08-17 14:25:57 -0400

    updates to travis

--- build.sh
+++ build.sh
@@ -15,8 +15,9 @@ rm -rf $TARGET
 mkdir $TARGET
 
 # build a golang container to build the binary
+GOVERSION="1.10"
 GOLANG_CONTAINER_ROOT="/go/src/github.com/chrisruffalo/gyip"
-GOLANG_CONTAINER=$(buildah from golang:1.9-alpine)
+GOLANG_CONTAINER=$(buildah from golang:${GOVERSION}-alpine)
 buildah umount $GOLANG_CONTAINER # ensure unmounted
 buildah run $GOLANG_CONTAINER -- mkdir -p $GOLANG_CONTAINER_ROOT{,/command}
 buildah config --workingdir "${GOLANG_CONTAINER_ROOT}" --env CGO_ENABLED="0" $GOLANG_CONTAINER
@@ -33,8 +34,8 @@ buildah rm $GOLANG_CONTAINER # clean up
 # build the actual container by shoving the binary into a minimal alpine container
 GYIP_CONTAINER=$(buildah from scratch)
 GYIP_CONTAINER_TAG="gyip/gyip"
-buildah copy $GYIP_CONTAINER "$TARGET/gyip" /gyip
 buildah config --port 8053 --workingdir "/" --entrypoint '["/gyip"]' $GYIP_CONTAINER
+buildah copy $GYIP_CONTAINER "$TARGET/gyip" /gyip
 buildah commit $GYIP_CONTAINER $GYIP_CONTAINER_TAG
 buildah rm $GYIP_CONTAINER # remove working container
 
